19b6d3b8413a193869ebdfd6170a72f4b3016f06
shamu92 <altshteyn@gmail.com>
2017-03-28 19:54:17 -0700

    Finished up to 0402

--- Ch04/04_01/start/src/index.js
+++ Ch04/04_01/start/src/index.js
@@ -1,13 +1,31 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { SkiDayCount } from './components/SkiDayCount'
+import { SkiDayList } from './components/SkiDayList'
 
 window.React = React
 
 render(
-	<SkiDayCount total={50}
-				 powder={20}
-				 backcountry={10}
-				 goal={100}/>,
+	<SkiDayList days={
+		[
+			{
+				resort:"Squaw Valley",
+				date: new Date("1/2/2016"),
+				powder: true,
+				backcountry: false
+			},
+			{
+				resort:"Mammoth",
+				date: new Date("2/24/2016"),
+				powder: true,
+				backcountry: false
+			},
+			{
+				resort:"Whistler",
+				date: new Date("3/22/2016"),
+				powder: true,
+				backcountry: true
+			}
+		]
+	}/>,
 	document.getElementById('react-container')
 )
